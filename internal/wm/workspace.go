fdccb3e7ebf38fa6b6d5f929d1c17ce073204435
joaosimsic <jpsimsic@hotmail.com>
2026-08-18 11:38:48 -0300

    feat: wm workspace

--- internal/wm/workspace.go
+++ internal/wm/workspace.go
@@ -1,6 +1,29 @@
 package wm
 
 type Workspace struct {
-    id int
-    list []*Client
+	id   int
+	list []*Client
+}
+
+func (w *Workspace) add(c *Client) {
+	w.list = append(w.list, c)
+}
+
+func (w *Workspace) remove(c *Client) {
+	i, ok := w.index(c)
+	if !ok {
+		return
+	}
+
+	w.list = append(w.list[:i], w.list[i+1:]...)
+}
+
+func (w *Workspace) index(c *Client) (int, bool) {
+	for i, client := range w.list {
+		if client == c {
+			return i, true
+		}
+	}
+
+	return 0, false
 }
